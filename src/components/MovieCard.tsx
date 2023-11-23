97db648b0d77ad5d9fe9131697fbc0dfc401f49d
Diego Vargas <yeyox1945@gmail.com>
2023-11-23 16:16:12 -0600

    - pager view installed

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,36 +4,45 @@ import { router } from "expo-router";
 
 interface Props {
   movie: Movie;
-  image?: string; 
+  image?: string;
   height?: number;
   width?: number;
 }
 
-const MovieCard = ({ movie, image = movie.poster_path, height = 150, width = 100 }: Props) => {
+const MovieCard = ({
+  movie,
+  image = movie.poster_path,
+  height = 150,
+  width = 100,
+}: Props) => {
   const uri = `https://image.tmdb.org/t/p/w500${image}`;
 
+  // actions
   const navigateToDetail = () =>
     router.push({
       pathname: "movies/[id]",
       params: {
         id: movie.id,
+        movie: movie,
       },
     });
 
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={navigateToDetail}>
-      <View style={{ height, width }}>
-        <Image
-          source={{
-            uri,
-          }}
-          style={{
-            flex: 1,
-            objectFit: "cover",
-            borderRadius: 20,
-          }}
-        />
-      </View>
+    <TouchableOpacity
+      activeOpacity={0.6}
+      onPress={navigateToDetail}
+      style={{ height, width }}
+    >
+      <Image
+        source={{
+          uri,
+        }}
+        style={{
+          flex: 1,
+          objectFit: "cover",
+          borderRadius: 20,
+        }}
+      />
     </TouchableOpacity>
   );
 };
